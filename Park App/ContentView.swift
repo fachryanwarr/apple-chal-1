a786bba10d8f3a0a5a2485ae2f2c404b653bd013
Vanessa <audreylia114@gmail.com>
2025-03-25 08:25:45 +0700

    landing page

--- Park App/ContentView.swift
+++ Park App/ContentView.swift
@@ -1,21 +1,60 @@
-//
-//  ContentView.swift
-//  Park App
-//
-//  Created by Vanessa Audreylia on 25/03/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing : 20)  {
+            Spacer()
+            
+            Text("Temukan Member \nSecure Parking")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            Image(systemName: "qrcode.viewfinder")
+                .resizable()
+                .scaledToFit( )
+                .frame(width: 150, height: 150)
+                .foregroundColor(.blue)
+                .padding(.vertical, 10)
+            
+            Text("Keamanan terbaik dimulai dengan langkah sederhana. Pindai, identifikasi, dan pastikan kendaraan terparkir sesuai dengan data.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 10)
+            Spacer()
+            
+            //tombol "scan plat nomor
+            Button(action: {
+                
+            }) {
+                Text("Scan Plat Nomor")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue.opacity(0.8))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 40)
+            
+            //tombol "List Daftar Member"
+            Button(action: {
+                
+            }) {
+                Text("List Daftar Member")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.gray.opacity(0.8))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                
+            }
+            .padding(.horizontal, 40)
+            
         }
-        .padding()
+        Spacer()
+        
     }
 }
 
