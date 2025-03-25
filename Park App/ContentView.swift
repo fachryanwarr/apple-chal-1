1676ca74574b5607587b0662af712648b606ec7e
Vanessa <audreylia114@gmail.com>
2025-03-25 08:06:16 +0700

    Initial Commit

---
+++ Park App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Park App
+//
+//  Created by Vanessa Audreylia on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Park App/Park_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Park_AppApp.swift
+//  Park App
+//
+//  Created by Vanessa Audreylia on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Park_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
